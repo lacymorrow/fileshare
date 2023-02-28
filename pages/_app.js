e6e3d78691b3f811ab348a80eb40dbc731b93ae4
Lacy Morrow <me@lacymorrow.com>
2023-02-27 20:23:52 -0500

    feat(ex): updates

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/global.scss'
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
